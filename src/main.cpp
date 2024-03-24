a6dd282e75c617184dd88bb55de595e59ebc43f4
Matt-Stedman <67828580+Matt-Stedman@users.noreply.github.com>
2024-03-24 11:56:45 +0000

    changed naming to -diy-

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 
 // OTA Hub via GitHub
 #define OTAGH_OWNER_NAME "Hard-Stuff"
-#define OTAGH_REPO_NAME "OTA-Hub-github_example"
-#include <OTA-Hub-github.hpp>
-// -- assumes we're using the default api.github.com/ server and ca_certs.
+#define OTAGH_REPO_NAME "OTA-Hub-diy-example_project"
+#include <OTA-Hub-diy.hpp>
 
 // Networking
 static const char *WIFI_SSID = "Hard-Stuff.com";
