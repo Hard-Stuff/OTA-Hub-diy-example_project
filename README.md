195624a88feb7da80578541dac1b39a00cd4d64c
Matt-Stedman <67828580+Matt-Stedman@users.noreply.github.com>
2024-03-24 11:21:47 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example repository for OTA Hub's 
---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+
+// OTA Hub via GitHub
+#define OTAGH_OWNER_NAME "Hard-Stuff"
+#define OTAGH_REPO_NAME "OTA-Hub-github_example"
+#include <OTA-Hub-github.hpp>
+// -- assumes we're using the default api.github.com/ server and ca_certs.
+
+// Networking
+static const char *WIFI_SSID = "Hard-Stuff.com";
+static const char *WIFI_PASS = "password";
+
+#include <WiFiClientSecure.h>
+WiFiClientSecure wifi_client;
+
+void setup()
+{
+    // Initialise our board
+    Serial.begin(115200);
+    Serial.println("Started...");
+
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+        Serial.println("WiFi failure");
+        ESP.restart();
+    }
+
+    // Initialise OTA
+    wifi_client.setCACert(OTAGH_CA_CERT); // Set the api.github.cm SSL cert on the WiFiSecure modem
+    OTA::init(wifi_client);
+
+    // Check OTA for updates
+    OTA::UpdateObject details = OTA::isUpdateAvailable();
+    details.print();
+    if (OTA::NO_UPDATE != details.condition)
+    {
+        Serial.println("An update is available!");
+        // Perform OTA update
+        OTA::InstallCondition result = OTA::performUpdate(&details);
+        // GitHub hosts files on different server, so we have to follow the redirect, unfortunately.
+        if (result == OTA::REDIRECT_REQUIRED)
+        {
+            wifi_client.setCACert(OTAGH_REDIRECT_CA_CERT); // Now set the objects.githubusercontent.com SSL cert
+            OTA::continueRedirect(&details);               // Follow the redirect and performUpdate.
+        }
+    }
+    else
+    {
+        Serial.println("No new update available. Continuing...");
+    }
+    Serial.print("Loop");
+}
+
+void loop()
+{
+    delay(5000);
+    Serial.print("edy loop");
+}
